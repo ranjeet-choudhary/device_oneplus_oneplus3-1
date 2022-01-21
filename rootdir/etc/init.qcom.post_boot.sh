689cf8cb98473ef8e41963739fa6c4532ee6d584
ranjeet-choudhary <ranjeet-choudhary@users.noreply.github.com>
2022-01-21 11:59:36 +0530

    #limit voltage_max to 4160, stops charging at 90%

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -5587,9 +5587,10 @@ case "$console_config" in
         echo "Enable console config to $console_config"
         ;;
 esac
-#limit voltage_max to 4170, stops charging at 90%
-echo 4170 > /sys/class/power_supply/battery/voltage_max
-echo 4170 > /sys/class/power_supply/usb/voltage_max
+
+#limit voltage_max to 4160, stops charging at 90%
+echo 4160000 > /sys/class/power_supply/battery/voltage_max
+echo 4160000 > /sys/class/power_supply/usb/voltage_max
 
 # Parse misc partition path and set property
 misc_link=$(ls -l /dev/block/bootdevice/by-name/misc)
